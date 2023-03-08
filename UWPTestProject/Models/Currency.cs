2ba41692aaa2fdd7438f5f8cc31c1e50a50a7178
Vladyslav <harkavenko.vladyslav.00@gmail.com>
2023-03-08 12:45:09 +0200

    Added MVVM arcitecture. Added Currency model and view model.

---
+++ UWPTestProject/Models/Currency.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWPTestProject.Models
+{
+    internal class Currency
+    {
+        public string ID { get; set; }
+        public int Rank { get; set; }
+        public string Symbol { get; set; }
+        public string Name { get; set; }
+        public double PriceUsd { get; set; }
+    }
+}
---
+++ UWPTestProject/ViewModels/CurrencyViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UWPTestProject.Models;
+
+namespace UWPTestProject.ViewModels
+{
+    internal class CurrencyViewModel
+    {
+        public CurrencyViewModel() 
+        {
+            Currencies = new ObservableCollection<Currency>();
+            Currencies.Add(new Currency() { ID = "Bitcoin", Name = "Bitcoin", Rank = 1, Symbol = "bit", PriceUsd = 19.28 });
+        }
+        public ObservableCollection<Currency> Currencies { get; set; }
+    }
+}
